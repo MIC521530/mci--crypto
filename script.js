2f3d50651432513c12dbe7869d0f3a4bfdb09793
WU <love521530@gmail.com>
2025-07-13 17:28:53 +0800

    Update script.js
    Signed-off-by: WU <love521530@gmail.com>

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
 document.getElementById("twd-amount").addEventListener("input", async function () {
   const amount = this.value;
   const coin = document.getElementById("crypto-select").value;
-  if (!amount) return;
-  const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin.toLowerCase()}&vs_currencies=twd`);
+  if (!amount || !coin) return;
+
+  const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=twd`);
   const data = await res.json();
-  const rate = data[coin.toLowerCase()].twd;
+  const rate = data[coin].twd;
   const converted = (amount / rate).toFixed(6);
-  document.getElementById("converted-price").innerText = `預估可兌換：${converted} ${coin}`;
+
+  document.getElementById("converted-price").innerText = `預估可兌換：${converted} ${coin.toUpperCase()}`;
 });
