7b358bf4910f25255584343526a045618da60d55
WU <love521530@gmail.com>
2025-07-13 15:28:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("twd-amount").addEventListener("input", async function () {
+  const amount = this.value;
+  const coin = document.getElementById("crypto-select").value;
+  if (!amount) return;
+  const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coin.toLowerCase()}&vs_currencies=twd`);
+  const data = await res.json();
+  const rate = data[coin.toLowerCase()].twd;
+  const converted = (amount / rate).toFixed(6);
+  document.getElementById("converted-price").innerText = `預估可兌換：${converted} ${coin}`;
+});
